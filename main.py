fdf7f024b555513111440aeb1b4f1689ec5d0c89
Phoenix67rus <tm87rus67@gmail.com>
2023-12-20 18:56:34 +0300

    Исправить функцию login()
    Переименовано псевдоним login на verify
    Поэтапный ввод логина и пароля
    Функция registration() запускается после полного прохождения цикла for
    
    Исправить функцию registration()
    При совпадении имен пользователя предлагает ввести новое имя

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import os
 
-def start():
+
+def main():
     try:
-        choice = int(input('Для входа выберите - 1,\n для регистрации - 2,\n для выхода  - 3\n '))
+        choice = int(input('Для входа выберите - 1,\nДля регистрации - 2,\nДля выхода  - 3\n'))
         if choice == 1:
             login()
         elif choice == 2:
-            registr()
+            registration()
         elif choice == 3:
             exit()
         else:
             print('Введенное число должно быть от 1 до 3!')
-            start()
+            main()
     except ValueError:
         print('Вы ввели буквы, а надо цифры!')
-        start()
+        main()
+
 
 def login():
     log = input('Введите имя: ')
-    with open('Base.txt', 'r', encoding='utf-8') as login:
+    with open('Base.txt', 'r', encoding='utf-8') as verify:
         size = os.path.getsize('Base.txt')
         if size == 0:
             print('Необходима регистрация!')
-            registr()
+            registration()
         else:
-            for i in login.readlines():
+            for i in verify.readlines():
                 user = i.split(' ')
                 if log == user[0]:
                     pas = input('Введите пароль: ')
@@ -32,44 +34,38 @@ def login():
                         print(f'Wellcome, {user[0]}')
                         exit()
                     else:
-                        print('Введен не верный пароль! \n')
-                        main()
-                elif log != user[0]:
-                    pass
-                else:
-                    print('Необходима регистрация!')
-                    registr()
+                        print('Пара логин/ пароль не совпадает! \n')
+                        login()
+            print('Необходима регистрация!')
+            registration()
 
 
-def registr():
-    userreg = []
+def registration():
+    user_reg = []
     log = input('Введите имя: ')
-    with open('Base.txt', 'r', encoding='utf-8') as login:
-        for i in login.readlines():
+    with open('Base.txt', 'r', encoding='utf-8') as base:
+        for i in base.readlines():
             user = i.split(' ')
             if log == user[0]:
                 print('Такое имя занято! Введите другое! \n')
-                registr()
+                registration()
             else:
                 continue
     if 3 < len(log) < 20:
-        userreg.append(log)
+        user_reg.append(log)
     else:
         print('Логин должен быть больше 3 но меньше 20 символов! Повторите ввод! \n')
-        registr()
+        registration()
     pas = input('Введите пароль: ')
     if 4 < len(pas) < 32:
-        userreg.append(pas)
+        user_reg.append(pas)
     else:
         print('Пароль должен быть больше 4 но меньше 32 символов! Повторите ввод! \n')
-        registr()
-    data = ' '.join(userreg)
-    with open('Base.txt', 'a', encoding='utf-8') as login:
-        login.write(f'{data} \n')
-    start()
-
-def main():
-    start()
+        registration()
+    data = ' '.join(user_reg)
+    with open('Base.txt', 'a', encoding='utf-8') as base:
+        base.write(f'{data} \n')
+    main()
 
 
 if __name__ == '__main__':

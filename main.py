133f04113393a57fd201f79faaee66ca07d078f9
Phoenix67rus <tm87rus67@gmail.com>
2023-12-21 21:36:24 +0300

    Лучше не использовать список user, он лишний; Убрал не используемый список Исправлены не удачные переменные registr, log, pas Для промежутков использовал range main() отправил вниз программы сделал проверку файла, если файл не существует программа его создает

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 import os
 
 
+def authorization():
+    login = input('Введите имя: ')
+    try:
+        with open('Base.txt', 'r', encoding='utf-8') as verify:
+            size = os.path.getsize('Base.txt')
+            if size == 0:
+                print('Необходима регистрация!')
+                registration()
+            else:
+                for i in verify.readlines():
+                    user = i.split(' ')
+                    if login == user[0]:
+                        password = input('Введите пароль: ')
+                        if password == user[1]:
+                            print(f'Wellcome, {user[0]}')
+                            exit()
+                        else:
+                            print('Пара логин/ пароль не совпадает! \n')
+                            authorization()
+                print('Необходима регистрация!')
+                registration()
+    except FileNotFoundError:
+        with open('Base.txt', 'a', encoding='utf-8'):
+            pass
+        authorization()
+
+
+def registration():
+    login = input('Введите имя: ')
+    with open('Base.txt', 'r', encoding='utf-8') as base:
+        for i in base.readlines():
+            user = i.split(' ')
+            if login == user[0]:
+                print('Такое имя занято! Введите другое! \n')
+                registration()
+            else:
+                continue
+    if not len(login) in range(3, 21):
+        print('Логин должен быть больше 3 но меньше 20 символов! Повторите ввод! \n')
+        registration()
+    password = input('Введите пароль: ')
+    if not len(password) in range(4, 33):
+        print('Пароль должен быть больше 4 но меньше 32 символов! Повторите ввод! \n')
+        registration()
+    with open('Base.txt', 'a', encoding='utf-8') as base:
+        base.write(f'{login} {password} \n')
+    main()
+
+
 def main():
     try:
         choice = int(input('Для входа выберите - 1,\nДля регистрации - 2,\nДля выхода  - 3\n'))
         if choice == 1:
-            login()
+            authorization()
         elif choice == 2:
             registration()
         elif choice == 3:
@@ -16,56 +65,6 @@ def main():
     except ValueError:
         print('Вы ввели буквы, а надо цифры!')
         main()
-
-
-def login():
-    log = input('Введите имя: ')
-    with open('Base.txt', 'r', encoding='utf-8') as verify:
-        size = os.path.getsize('Base.txt')
-        if size == 0:
-            print('Необходима регистрация!')
-            registration()
-        else:
-            for i in verify.readlines():
-                user = i.split(' ')
-                if log == user[0]:
-                    pas = input('Введите пароль: ')
-                    if pas == user[1]:
-                        print(f'Wellcome, {user[0]}')
-                        exit()
-                    else:
-                        print('Пара логин/ пароль не совпадает! \n')
-                        login()
-            print('Необходима регистрация!')
-            registration()
-
-
-def registration():
-    user_reg = []
-    log = input('Введите имя: ')
-    with open('Base.txt', 'r', encoding='utf-8') as base:
-        for i in base.readlines():
-            user = i.split(' ')
-            if log == user[0]:
-                print('Такое имя занято! Введите другое! \n')
-                registration()
-            else:
-                continue
-    if 3 < len(log) < 20:
-        user_reg.append(log)
-    else:
-        print('Логин должен быть больше 3 но меньше 20 символов! Повторите ввод! \n')
-        registration()
-    pas = input('Введите пароль: ')
-    if 4 < len(pas) < 32:
-        user_reg.append(pas)
-    else:
-        print('Пароль должен быть больше 4 но меньше 32 символов! Повторите ввод! \n')
-        registration()
-    data = ' '.join(user_reg)
-    with open('Base.txt', 'a', encoding='utf-8') as base:
-        base.write(f'{data} \n')
-    main()
 
 
 if __name__ == '__main__':
